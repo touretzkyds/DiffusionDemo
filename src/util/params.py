5a5aa820e8076c157f7c07d8f08c5ede84f00c56
akameswa <adithya.k.nitt@gmail.com>
2024-01-23 14:36:32 -0500

    update params.py

--- src/util/params.py
+++ src/util/params.py
@@ -5,32 +5,41 @@ import tntn
 
 torch_device = "cuda" if torch.cuda.is_available() else "cpu"
 
-HF_ACCESS_TOKEN = ""                                                           
+isLCM = False
+isLocal = False
+HF_ACCESS_TOKEN = ""        
 
-model_path = "Lykon/dreamshaper-8"                                             
-imageHeight, imageWidth = 512, 512                                           
-guidance_scale = 8                                                             
+model_path = "segmind/small-sd"    
+prompt = "Self-portrait oil painting, a beautiful cyborg with golden hair, 8k"
+promptA = "Self-portrait oil painting, a beautiful man with golden hair, 8k"
+promptB = "Self-portrait oil painting, a beautiful woman with golden hair, 8k"
 
-prompt = "red balloon in the sky"
 num_images = 5                                                                  
-differentiation = 0.1                                                           
+differentiation = 1                                                           
 num_inference_steps = 8                                                        
-seed = 69420                                                                    
-intermediate = False                           
+seed = 69420                                                              
 
+guidance_scale = 8                                                                 
+intermediate = True                           
 pokeX, pokeY = 256, 256                                                         
 pokeHeight, pokeWidth = 128, 128                                                
-
-promptA = "a car driving on a highway in a city"
-promptB = "a truck driving on a highway in a city"
+imageHeight, imageWidth = 512, 512  
 
 tokenizer = CLIPTokenizer.from_pretrained(model_path, subfolder="tokenizer")
 text_encoder = CLIPTextModel.from_pretrained(model_path, subfolder="text_encoder").to(torch_device)
-scheduler = LMSDiscreteScheduler.from_pretrained(model_path, subfolder="scheduler")
+
+if isLCM:
+    scheduler = LCMScheduler.from_pretrained(model_path, subfolder="scheduler")
+else:
+    scheduler = LMSDiscreteScheduler.from_pretrained(model_path, subfolder="scheduler")
+
 unet = UNet2DConditionModel.from_pretrained(model_path, subfolder="unet").to(torch_device)
 vae = AutoencoderKL.from_pretrained(model_path, subfolder="vae").to(torch_device)
 
-dash_tunnel = tntn.bore(8000).tunnel
+if isLocal: 
+    dash_tunnel = "http://127.0.0.1:8000/"
+else:
+    dash_tunnel = tntn.bore(8000).tunnel
 
 __all__ = [
     "prompt", 
