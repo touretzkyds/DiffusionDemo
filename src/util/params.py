6c06948cffb91db1200c818246e005163c8b22e7
akameswa <adithya.k.nitt@gmail.com>
2024-03-22 18:01:00 -0400

    misc: change scheduler to ddim reason: https://www.felixsanz.dev/articles/complete-guide-to-samplers-in-stable-diffusion#ddim

--- src/util/params.py
+++ src/util/params.py
@@ -6,7 +6,7 @@ from diffusers import (
     AutoencoderKL,
     UNet2DConditionModel,
     LCMScheduler,
-    LMSDiscreteScheduler,
+    DDIMScheduler,
     StableDiffusionPipeline,
 )
 
@@ -43,7 +43,7 @@ text_encoder = CLIPTextModel.from_pretrained(model_path, subfolder="text_encoder
 if isLCM:
     scheduler = LCMScheduler.from_pretrained(model_path, subfolder="scheduler")
 else:
-    scheduler = LMSDiscreteScheduler.from_pretrained(model_path, subfolder="scheduler")
+    scheduler = DDIMScheduler.from_pretrained(model_path, subfolder="scheduler")
 
 unet = UNet2DConditionModel.from_pretrained(model_path, subfolder="unet").to(
     torch_device
