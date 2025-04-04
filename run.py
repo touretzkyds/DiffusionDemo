634a503e0b11b5ae34922c3ab21c72bf207bee53
akameswa <adithya.k.nitt@gmail.com>
2025-04-04 23:10:06 +0000

    inpainting: add default color

--- run.py
+++ run.py
@@ -502,7 +502,7 @@ with gr.Blocks(css="#step_size_circular {background-color: #666666} #step_size_c
                 with gr.Column():
                     # Uploaded image input
                     uploaded_img_inpaint = gr.Sketchpad(
-                        sources="upload", brush=gr.Brush(colors=["#ffff00"]), type="pil", label="Upload"
+                        sources="upload", brush=gr.Brush(colors=["#ffff00"], default_color="#ffff00"), type="pil", label="Upload"
                     )
                     # Text prompt input
                     prompt_inpaint = gr.Textbox(
